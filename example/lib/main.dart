1a0bbfed4381b1772eb8bc94c72f844d6b29798a
tnkfactory <tech@tnkfactory.com>
2025-09-24 21:53:43 +0900

    ios 플러그인 오류 수정

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -33,8 +33,8 @@ class _MyAppState extends State<MyApp> {
     // Platform messages may fail, so we use a try/catch PlatformException.
     // We also handle the message potentially returning null.
     try {
-      platformVersion =
-          await _tnkRwdNVadPlugin.getPlatformVersion() ?? 'Unknown platform version';
+      _tnkRwdNVadPlugin.setUserName("asdf");
+      platformVersion = await _tnkRwdNVadPlugin.getPlatformVersion() ?? 'Unknown platform version';
     } on PlatformException {
       platformVersion = 'Failed to get platform version.';
     }
@@ -49,15 +49,167 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
+  String _tnkResult = "";
+
+  Future<void> showAdList() async {
+    // _tnkRwdNVadPlugin.showATTPopup();
+    _tnkRwdNVadPlugin.setCOPPA(false);
+    _tnkRwdNVadPlugin.setUserName("tnk_test");
+    // String platformVersion;
+    //
+    // try {
+    //   await _tnkRwdNVadPlugin.setUserName("testUser");
+    //   await _tnkRwdNVadPlugin.setCOPPA(false);
+    //
+    //   _tnkRwdNVadPlugin.setUseTermsPopup(false);
+    //   // _tnkFlutterRwdPlugin.setCategoryAndFilter(4, 0);
+    //   platformVersion = await _tnkRwdNVadPlugin.showAdList("미션 수행하기") ?? 'Unknown platform version';
+    // } on PlatformException {
+    //   platformVersion = 'Failed to get platform version.';
+    // }
+    //
+    // // sleepAndClose();
+    // if (!mounted) return;
+    //
+    // setState(() {
+    //   _tnkResult = platformVersion;
+    // });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+        body: Stack(
+          children: [
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      DataTable(
+                        columns: const [DataColumn(label: Text('결과'))],
+                        rows: [
+                          DataRow(cells: [DataCell(Text(_tnkResult))]),
+                        ],
+                      ),
+                    ],
+                  ),
+                  OverflowBar(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          // ElevatedButton(
+                          //   onPressed: () {
+                          //     getJsonAdList();
+                          //   },
+                          //   style: ElevatedButton.styleFrom(
+                          //       foregroundColor: Colors.white,
+                          //       backgroundColor: Colors.redAccent,
+                          //       shadowColor: Colors.redAccent,
+                          //       elevation: 5),
+                          //   child: const Text("TEST"),
+                          // ),
+                          ElevatedButton(
+                            onPressed: () {
+                              showAdList();
+                            },
+                            style: ElevatedButton.styleFrom(foregroundColor: Colors.white, backgroundColor: Colors.blueAccent, shadowColor: Colors.blueAccent, elevation: 10),
+                            child: const Text('내 포인트'),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {
+                              showAdList();
+                            },
+                            style: ElevatedButton.styleFrom(foregroundColor: Colors.white, backgroundColor: Colors.blueAccent, shadowColor: Colors.blueAccent, elevation: 10),
+                            child: const Text('적립가능한 포인트'),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {
+                              // purchaseItem(_itemId, _cost);
+                            },
+                            style: ElevatedButton.styleFrom(foregroundColor: Colors.white, backgroundColor: Colors.redAccent, shadowColor: Colors.redAccent, elevation: 10),
+                            child: const Text('테스트구매'),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {
+                              // withdrawPoints("테스트 인출");
+                            },
+                            style: ElevatedButton.styleFrom(foregroundColor: Colors.white, backgroundColor: Colors.redAccent, shadowColor: Colors.redAccent, elevation: 10),
+                            child: const Text('테스트인출'),
+                          ),
+                        ],
+                      ),
+
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {
+                              // presentAdDetailView(227796);
+
+                              // onAdItemClick("726941");
+                            },
+                            style: ElevatedButton.styleFrom(foregroundColor: Colors.white, backgroundColor: Colors.redAccent, shadowColor: Colors.redAccent, elevation: 10),
+                            child: const Text('광고상세 진입'),
+                          ),
+
+                          ElevatedButton(
+                            onPressed: () {
+                              // setUserName();
+                              // onAdItemClick("726941");
+                            },
+                            style: ElevatedButton.styleFrom(foregroundColor: Colors.white, backgroundColor: Colors.redAccent, shadowColor: Colors.redAccent, elevation: 10),
+                            child: const Text('set username'),
+                          ),
+                        ],
+                      ),
+
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {
+                              // adJoin(227796);
+                            },
+                            style: ElevatedButton.styleFrom(foregroundColor: Colors.white, backgroundColor: Colors.purpleAccent, shadowColor: Colors.purpleAccent, elevation: 10),
+                            child: const Text('Ad Join'),
+                          ),
+
+                          ElevatedButton(
+                            onPressed: () {
+                              // adAction(227796);
+                            },
+                            style: ElevatedButton.styleFrom(foregroundColor: Colors.white, backgroundColor: Colors.purpleAccent, shadowColor: Colors.purpleAccent, elevation: 10),
+                            child: const Text('Ad Action'),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Positioned(
+              bottom: 16,
+              right: 16,
+              child: FloatingActionButton(
+                onPressed: () {
+                  // 원하는 동작 작성
+                  showAdList();
+                },
+                child: const Column(mainAxisSize: MainAxisSize.min, mainAxisAlignment: MainAxisAlignment.center, children: [Icon(Icons.tv, size: 20), SizedBox(height: 4), Text("오퍼월", style: TextStyle(fontSize: 10))]),
+              ),
+            ),
+          ],
         ),
       ),
     );
--- ios/Classes/TnkFlutterRwdPlugin.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#import <Flutter/Flutter.h>
-
-@interface TnkFlutterRwdPlugin : NSObject<FlutterPlugin>
-@end
